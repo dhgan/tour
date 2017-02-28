ed22f4f6fdb45d53e41d0c64ad6595365ac9e124
dhgan <1562510684@qq.com>
2017-02-28 21:51:09 +0800

    项目启动方式修改"

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
-var os = require('os');
 var spawn = require('child_process').spawn;
 
 var devPort = 8888,
 	serverPort = 8000;
 
-var cmdStr = '';
-
-// 判断系统
-if(os.platform().toLowerCase().indexOf('win32') > -1) {
-	// windows系统
-	cmdStr = 'set PORT=' + serverPort + ' & set DEBUG=tour & node ./bin/www';
-} else {
-	cmdStr = 'PORT=' + serverPort + ' DEBUG=tour node ./bin/www';
-}
-
-var ls = spawn('node', ['./bin/www']);
+var ls = spawn('node', ['./bin/www'], {
+	env: {
+		PORT: serverPort,
+		DEBUG: "tour"
+	}
+});
 
 ls.stdout.on('data', (data) => {
   console.log(`${data}`);
